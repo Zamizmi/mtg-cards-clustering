daf11a459ea1fc6f4ced041e6cf8f84d01cee0c2
Sami Lindqvist <sami.k.lindqvist@gmail.com>
2022-11-03 09:07:06 +0200

    elbow

--- mtg_card_clustering.py
+++ mtg_card_clustering.py
@@ -38,8 +38,14 @@ vectorizer = TfidfVectorizer(
 
 features = vectorizer.fit_transform(data["oracle_text"])
 
-k = 48
-model = KMeans(n_clusters=k, init="k-means++", max_iter=100, n_init=1)
+k = 10
+kmin = 100
+kmax = 150
+init = "k-means++"
+max_iter = 300
+n_init = 10
+
+model = KMeans(n_clusters=k, init=init, max_iter=max_iter, n_init=n_init)
 model.fit(features)
 
 non_pw_legal_df["cluster"] = model.labels_
@@ -60,11 +66,11 @@ print("Cluster centroids: \n")
 order_centroids = model.cluster_centers_.argsort()[:, ::-1]
 terms = vectorizer.get_feature_names_out()
 
-for i in range(k):
-    print("Cluster %d:" % i)
-    for j in order_centroids[i, :10]:  # print out 10 feature terms of each cluster
-        print(" %s" % terms[j])
-    print("------------")
+# for i in range(k):
+#     print("Cluster %d:" % i)
+#     for j in order_centroids[i, :10]:  # print out 10 feature terms of each cluster
+#         print(" %s" % terms[j])
+#     print("------------")
 
 
 def calculate_silhoutte_score(points, kmin: int, kmax: int):
@@ -74,23 +80,28 @@ def calculate_silhoutte_score(points, kmin: int, kmax: int):
     # dissimilarity would not be defined for a single cluster, thus, minimum number of clusters should be 2
     for k in range(kmin, kmax):
         print(f"----- Here we go: {k}")
-        kmeans = KMeans(n_clusters=k).fit(points)
+        kmeans = KMeans(n_clusters=k, init=init, max_iter=max_iter, n_init=n_init).fit(
+            points
+        )
         labels = kmeans.labels_
         sil.append(silhouette_score(points, labels, metric="euclidean"))
 
     return sil
 
 
-def calculate_WSS(points, kmax: int):
+def calculate_WSS(points, kmin: int, kmax: int):
     sse = []
-    for k in range(1, kmax + 1):
-        kmeans = KMeans(n_clusters=k, init="k-means++", n_init=1).fit(points)
+    for k in range(kmin, kmax):
+        print(f"----- Here we go: {k}")
+        kmeans = KMeans(n_clusters=k, init=init, max_iter=max_iter, n_init=n_init).fit(
+            points
+        )
         centroids = kmeans.cluster_centers_
         pred_clusters = kmeans.predict(points)
         curr_sse = 0
 
         # calculate square of Euclidean distance of each point from its cluster center and add to current WSS
-        for i in range(len(points)):
+        for i in range(points.shape[0]):
             curr_center = centroids[pred_clusters[i]]
             curr_sse += (points[i, 0] - curr_center[0]) ** 2 + (
                 points[i, 1] - curr_center[1]
@@ -100,8 +111,48 @@ def calculate_WSS(points, kmax: int):
     return sse
 
 
-# silhouette_scores = calculate_silhoutte_score(features, 45, 56)
-# print("silhoutte_score with k from 45 to 56", silhouette_scores)
+# # silhouette_scores = calculate_silhoutte_score(features, kmin, kmax)
 
-# plt.plot(silhouette_scores)
+
+# wss_scores = calculate_WSS(features, kmin, kmax)
+
+# plt.plot(wss_scores, "o-", color="green", label="WSS")
+# # plt.plot(silhouette_scores, "o-", color="blue", label="Silhouette")
+
+# plt.title(f"WSS and Silhouette scores between k={kmin} and k={kmax}")
+# plt.legend(loc="lower right")
 # plt.show()
+
+
+def elbow_method(Y_sklearn):
+    """
+    This is the function used to get optimal number of clusters in order to feed to the k-means clustering algorithm.
+    """
+
+    number_clusters = range(
+        kmin, kmax
+    )  # Range of possible clusters that can be generated
+    kmeans = [
+        KMeans(n_clusters=i, init=init, max_iter=max_iter, n_init=n_init).fit(Y_sklearn)
+        for i in number_clusters
+    ]  # Getting no. of clusters
+
+    score = [
+        kmeans[i].fit(Y_sklearn).score(Y_sklearn) for i in range(len(kmeans))
+    ]  # Getting score corresponding to each cluster.
+    score = [i * -1 for i in score]  # Getting list of positive scores.
+    return (number_clusters, score)
+
+
+val1 = elbow_method(features)
+fig, ax = plt.subplots()
+ax.set_xlim(kmin - 1, kmax)
+ax.plot(val1[0], val1[1], color="green")
+
+plt.xlabel("Number of Clusters")
+plt.ylabel("Score")
+plt.title("Elbow Method")
+plt.show()
+
+
+elbow_method(features)
